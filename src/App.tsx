ab3848cdee592c345668d705f57791be237d3cde
Iva Zhelyazkova <iva.a.zhelyazkova@gmail.com>
2024-12-03 12:48:21 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+// import AWS from "aws-sdk";
+
+function App() {
+  return (
+    <>
+      <div>File system</div>
+    </>
+  );
+}
+
+export default App;
